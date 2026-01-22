c22529ce4f81c6e520835f7e021d360e32ca6116
seunghee <victoria9981@naver.com>
2026-01-22 19:35:44 +0900

    feat: 패키지 이름 불일치 오류 수정

---
+++ app/src/main/java/com/example/nonggleresume/App.kt
@@ -0,0 +1,14 @@
+package com.example.nonggleresume
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+      KakaoSdk.init(this, "kakao${BuildConfig.KAKAO_NATIVE_APP_KEY}")
+    }
+}
