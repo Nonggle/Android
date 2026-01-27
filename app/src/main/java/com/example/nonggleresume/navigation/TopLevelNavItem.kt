403a0994ecbecb11ab0765d235876a5160585cd9
seunghee <victoria9981@naver.com>
2026-01-27 20:07:56 +0900

    feat: 네비게이션 아이템 데이터형 수정

--- app/src/main/java/com/example/nonggleresume/navigation/TopLevelNavItem.kt
+++ app/src/main/java/com/example/nonggleresume/navigation/TopLevelNavItem.kt
@@ -1,43 +1,36 @@
 package com.example.nonggleresume.navigation
 
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.example.designsystem.icon.NonggleIcons
 import com.example.download.navigation.DownLoadNavKey
 import com.example.home.navigation.HomeNavKey
 import com.example.setting.navigation.SettingNavKey
-import com.example.home.R as homeResource
-import com.example.download.R as downloadResource
-import com.example.setting.R as settingResource
+import com.example.nonggleresume.R
 
 data class TopLevelNavItem(
     @DrawableRes val selectedIconRes: Int,
     @DrawableRes val unselectedIconRes: Int,
-    @StringRes val iconTextId: Int,
     val title: @Composable () -> String
 )
 
 val HOME = TopLevelNavItem(
     selectedIconRes = NonggleIcons.homeSelected,
     unselectedIconRes = NonggleIcons.homeUnselected,
-    iconTextId = homeResource.string.feature_name,
-    title = { stringResource(homeResource.string.feature_name) }
+    title = { stringResource(R.string.nav_item_home) }
 )
 
 val DOWNLOAD = TopLevelNavItem(
     selectedIconRes = NonggleIcons.downloadSelected,
     unselectedIconRes = NonggleIcons.downloadUnselected,
-    iconTextId = downloadResource.string.feature_name,
-    title = { stringResource(downloadResource.string.feature_name) }
+    title = { stringResource(R.string.nav_item_download) }
 )
 
 val SETTING = TopLevelNavItem(
     selectedIconRes = NonggleIcons.settingSelected,
     unselectedIconRes = NonggleIcons.settingUnselected,
-    iconTextId = settingResource.string.feature_name,
-    title = { stringResource(settingResource.string.feature_name) }
+    title = { stringResource(R.string.nav_item_setting) }
 )
 
 val TOP_LEVEL_NAV_ITEMS = mapOf(
