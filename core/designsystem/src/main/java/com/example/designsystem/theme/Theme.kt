6e34ef921984b1e27965928392bbc95d3201e70c
seunghee <victoria9981@naver.com>
2026-01-31 01:36:35 +0900

    feat: background theme 정의

--- core/designsystem/src/main/java/com/example/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/example/designsystem/theme/Theme.kt
@@ -1,12 +1,43 @@
 package com.example.core.designsystem.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
+
 internal val LocalNonggleColors = staticCompositionLocalOf { LightNonggleColors }
 internal val LocalNonggleTypography = staticCompositionLocalOf { NonggleTypography() }
+
+private fun nonggleToMaterialColors(nonggleColors: NonggleColors) = lightColorScheme(
+    primary = nonggleColors.m1,
+    secondary = nonggleColors.s1,
+    background = nonggleColors.background,
+    surface = nonggleColors.bg,
+    error = nonggleColors.error,
+    onPrimary = nonggleColors.white,
+    onSecondary = nonggleColors.black,
+    onBackground = nonggleColors.g1,
+    onSurface = nonggleColors.g2,
+    onError = nonggleColors.white,
+)
+
+private fun nonggleToMaterialDarkColors(nonggleColors: NonggleColors) = darkColorScheme(
+    primary = nonggleColors.m1,
+    secondary = nonggleColors.s1,
+    background = nonggleColors.background,
+    surface = nonggleColors.bg,
+    error = nonggleColors.error,
+    onPrimary = nonggleColors.black,
+    onSecondary = nonggleColors.white,
+    onBackground = nonggleColors.g1,
+    onSurface = nonggleColors.g2,
+    onError = nonggleColors.black,
+)
+
 /**
  * 앱 전역에서 테마 속성에 접근하기 위한 진입점(Entry Point) 객체입니다.
  */
@@ -37,11 +68,15 @@ fun NonggleTheme(
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) DarkNonggleColors else LightNonggleColors
+    val materialColorScheme = if (darkTheme) nonggleToMaterialDarkColors(colors) else nonggleToMaterialColors(colors)
 
     CompositionLocalProvider(
         LocalNonggleColors provides colors,
         LocalNonggleTypography provides typography
     ) {
-        content()
+        MaterialTheme(
+            colorScheme = materialColorScheme,
+            content = content
+        )
     }
 }
