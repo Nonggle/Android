674d7696ea086242a147adee96f315d412064c70
seunghee <victoria9981@naver.com>
2026-02-01 23:28:46 +0900

    feat: 이력서 작성시 사용될 컴포넌트 정의

--- feature/resume/impl/src/main/java/com/example/impl/component/CommonComponent.kt
+++ feature/resume/impl/src/main/java/com/example/impl/component/CommonComponent.kt
@@ -2,12 +2,17 @@ package com.example.feature.resume.impl.component
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -15,9 +20,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -37,29 +46,246 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.example.core.designsystem.component.ContainedButton
+import com.example.core.designsystem.component.FullButton
+import com.example.core.designsystem.component.NonggleBottomSheet
+import com.example.core.designsystem.component.NonggleIconButton
+import com.example.core.designsystem.component.NonggleTextField
 import com.example.core.designsystem.component.OutlinedButton
+import com.example.core.designsystem.component.TextFieldType
 import com.example.core.designsystem.theme.NonggleTheme
+import com.example.designsystem.component.Picker
+import com.example.designsystem.component.rememberPickerState
 import com.example.feature.resume.impl.R
 import com.example.feature.resume.impl.step1.Gender
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 
 @Composable
-fun genderSelectButton(
+fun genderSelectBox(
     modifier: Modifier = Modifier,
-    text: String,
-    selectGender: (Gender) -> Unit,
-    userGender: Gender,
-    isSelect: Boolean, // 현재 해당 버튼을 통해 성별 유형을 정했는지 여부
+    onSelectGender: (Gender) -> Unit,
+    selectGenderResult: Gender, // 현재 해당 버튼을 통해 성별 유형을 정했는지 여부
 ) {
-    OutlinedButton(
+    Row(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        OutlinedButton(
+            modifier = modifier
+                .padding(end = 16.dp)
+                .weight(1f),
+            titleText = stringResource(R.string.resume1Screen_label_women),
+            onClick = { onSelectGender(Gender.FEMALE) },
+            isSelect = selectGenderResult == Gender.FEMALE,
+        )
+        OutlinedButton(
+            modifier = modifier.weight(1f),
+            titleText = stringResource(R.string.resume1Screen_label_man),
+            onClick = { onSelectGender(Gender.MALE) },
+            isSelect = selectGenderResult == Gender.MALE,
+        )
+    }
+}
+
+// 날짜 선택 박스로 공통으로 쓰일 컴포넌트
+@Composable
+fun dateSelectBox(
+    hintText: String,
+    selectDate: String,
+    onClick: () -> Unit,
+    paddingValues: PaddingValues
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(paddingValues)
+            .border(
+                BorderStroke(1.dp, NonggleTheme.colorScheme.g_line),
+                shape = RoundedCornerShape(4.dp)
+            )
+            .clickable { onClick() }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp, horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = selectDate.ifEmpty { hintText },
+                style = NonggleTheme.typography.b1_main,
+                textAlign = TextAlign.Start
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Image(
+                painter = painterResource(id = R.drawable.date),
+                contentDescription = null,
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun birthDatePickerBottomSheet(
+    modifier: Modifier = Modifier,
+    sheetState: SheetState,
+    years: List<Int>,
+    months: List<Int>,
+    days: List<Int>,
+    selectBirthDate: (String) -> Unit,
+    onDismissRequest: () -> Unit,
+) {
+    val yearValues = remember { years }
+    val yearPickerState = rememberPickerState()
+    val monthValues = remember { months }
+    val monthPickerState = rememberPickerState()
+    val dayValues = remember { days }
+    val dayPickerState = rememberPickerState()
+
+    NonggleBottomSheet(
         modifier = modifier,
-        titleText = text,
-        onClick = { selectGender(userGender) },
-        isSelect = isSelect,
+        sheetState = sheetState,
+        onDismissRequest = onDismissRequest,
+        content = {
+            Column(
+                modifier = modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier.padding(bottom = 32.dp),
+                    text = stringResource(R.string.resume1Screen_birthDateTitle),
+                    textAlign = TextAlign.Start,
+                    style = NonggleTheme.typography.b1_main,
+                    color = NonggleTheme.colorScheme.black
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Picker(
+                        state = yearPickerState,
+                        items = yearValues,
+                        visibleItemsCount = 3,
+                        modifier = Modifier.weight(0.3f),
+                        textModifier = Modifier.padding(10.dp),
+                        unit = "년"
+                    )
+                    Picker(
+                        state = monthPickerState,
+                        items = monthValues,
+                        visibleItemsCount = 3,
+                        modifier = Modifier.weight(0.3f),
+                        textModifier = Modifier.padding(10.dp),
+                        unit = "월"
+                    )
+                    Picker(
+                        state = dayPickerState,
+                        items = dayValues,
+                        visibleItemsCount = 3,
+                        modifier = Modifier.weight(0.3f),
+                        textModifier = Modifier.padding(10.dp),
+                        unit = "일"
+                    )
+                    FullButton(
+                        modifier = Modifier.padding(top = 32.dp, bottom = 32.dp),
+                        onClick = {
+                            selectBirthDate("${yearPickerState.selectedItem}${monthPickerState.selectedItem}${dayPickerState.selectedItem}")
+                            onDismissRequest()
+                        },
+                        title = stringResource(R.string.resume1Screen_confirmBtnText)
+                    )
+                }
+            }
+        }
     )
+}
+
+@Composable
+fun certificateSelectBox(
+    modifier: Modifier = Modifier,
+    onClickExistCertificationInfo: (Boolean) -> Unit,
+    isCertificationExist: Boolean, // 현재 해당 버튼을 통해 성별 유형을 정했는지 여부
+) {
+    Row(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        OutlinedButton(
+            modifier = modifier
+                .padding(end = 16.dp)
+                .weight(1f),
+            titleText = stringResource(R.string.resume1Screen_label_havecertificate),
+            onClick = { onClickExistCertificationInfo(true) },
+            isSelect = isCertificationExist,
+        )
+        OutlinedButton(
+            modifier = modifier.weight(1f),
+            titleText = stringResource(R.string.resume1Screen_label_nocertificate),
+            onClick = { onClickExistCertificationInfo(false) },
+            isSelect = isCertificationExist,
+        )
+    }
+}
+
+@Composable
+fun certificationInput(
+    modifier: Modifier = Modifier,
+    certificationName: String,
+    certificationInput: (String) -> Unit,
+    addCertificationList: (String) -> Unit,
+    certificationList: List<String>,
+    removeCertificationItem: (Int) -> Unit
+) {
+    Column {
+        Row(
+            modifier = modifier.fillMaxWidth()
+        ) {
+            NonggleTextField(
+                modifier = Modifier.weight(7f),
+                textFieldType = TextFieldType.Standard,
+                value = certificationName,
+                onValueChange = certificationInput,
+                trailingIcon = {
+                    if (certificationName.isNotEmpty()) {
+                        NonggleIconButton(
+                            ImageResourceId = R.drawable.xcircle,
+                            onClick = { certificationInput("") }
+                        )
+                    }
+                },
+                hintTextResId = R.string.resume1Screen_certificateDetail_inputHintText,
+            )
+            ContainedButton(
+                modifier = Modifier.weight(3f),
+                onClick = { addCertificationList(certificationName) },
+                titleText = stringResource(R.string.resume1Screen_confirmBtnText),
+            )
+        }
+        if (certificationList.isNotEmpty()) {
+            LazyVerticalGrid(
+                modifier = Modifier.padding(top = 16.dp),
+                columns = GridCells.Adaptive(minSize = 128.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(
+                    count = certificationList.size,
+                    key = { index -> certificationList[index] }
+                ) { index ->
+                    certificationChipItem(
+                        title = certificationList[index],
+                        removeChip = { removeCertificationItem(index) }
+                    )
+                }
+            }
+        }
+    }
+
 }
 
 
@@ -102,95 +328,4 @@ fun certificationChipItem(
         }
     }
 }
-
-/// date spinner 구현을 위한 단일 스피너 컴포넌트
-
-@Composable
-fun Picker (
-    modifier: Modifier = Modifier,
-    state: PickerState = rememberPickerState(),
-    items: List<String>,
-    startIndex: Int = 0,
-    visibleItemsCount: Int = 3,
-    textModifier: Modifier = Modifier,
-) {
-    val visibleItemsMiddle = visibleItemsCount / 2
-    val listScrollCount = Integer.MAX_VALUE
-    val listScrollMiddle = listScrollCount / 2
-    val listStartIndex = listScrollMiddle - listScrollMiddle % items.size - visibleItemsMiddle + startIndex
-
-    fun getItem(index: Int) = items[index % items.size]
-
-    val listState = rememberLazyListState(initialFirstVisibleItemIndex = listStartIndex)
-    val flingBehavior = rememberSnapFlingBehavior(lazyListState = listState)
-
-    val itemHeightPixels = remember { mutableStateOf(0) }
-    val density = LocalDensity.current
-    val itemHeightDp = remember(itemHeightPixels.value) {
-        with(density) { itemHeightPixels.value.toDp() }
-    }
-    val dividerColor = NonggleTheme.colorScheme.white
-
-    val fadingEdgeGradient = remember {
-        Brush.verticalGradient(
-            0f to Color.Transparent,
-            0.5f to dividerColor,
-            1f to Color.Transparent
-
-        )
-    }
-
-    LaunchedEffect(listState) {
-        snapshotFlow { listState.firstVisibleItemIndex }
-            .map { index -> getItem(index + visibleItemsMiddle) }
-            .distinctUntilChanged()
-            .collect { item -> state.selectedItem = item }
-    }
-
-    Box(modifier = modifier) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(itemHeightDp * visibleItemsCount)
-                .fadingEdge(fadingEdgeGradient),
-            state = listState,
-            flingBehavior = flingBehavior,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            items(listScrollCount) { index ->
-                Text(
-                    modifier = Modifier
-                        .onSizeChanged{size -> itemHeightPixels.value = size.height}
-                        .then(textModifier),
-                    text = getItem(index),
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    style = NonggleTheme.typography.b1_main.copy(color = NonggleTheme.colorScheme.black),
-                )
-            }
-        }
-        HorizontalDivider(
-            modifier = Modifier.offset(y = itemHeightDp * visibleItemsMiddle),
-            color = NonggleTheme.colorScheme.m1,
-        )
-        HorizontalDivider(
-            modifier = Modifier.offset(y = itemHeightDp * (visibleItemsMiddle + 1)),
-            color = NonggleTheme.colorScheme.m1,
-        )
-    }
-}
-
-private fun Modifier.fadingEdge(brush: Brush) = this
-    .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
-    .drawWithContent {
-        drawContent()
-        drawRect(brush = brush, blendMode = BlendMode.DstIn)
-    }
-@Composable
-fun rememberPickerState() = remember { PickerState() }
-
-class PickerState {
-    var selectedItem by mutableStateOf("")
-}
-
 
