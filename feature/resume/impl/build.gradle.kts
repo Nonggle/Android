39c342b6cf6d2bc4c84517b85004efb9a4dcbcbc
seunghee <victoria9981@naver.com>
2026-02-04 23:35:09 +0900

    fix: material 버전 관련 이슈 해결

--- feature/resume/impl/build.gradle.kts
+++ feature/resume/impl/build.gradle.kts
@@ -11,8 +11,8 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.compose.foundation.layout)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
