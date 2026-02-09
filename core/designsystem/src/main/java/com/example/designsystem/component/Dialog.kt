f7a0bf674ee031bcf51b8fa1e88c18d83c30d893
seunghee <victoria9981@naver.com>
2026-02-09 20:53:31 +0900

    feat: calendar dialog 생성

--- core/designsystem/src/main/java/com/example/designsystem/component/Dialog.kt
+++ core/designsystem/src/main/java/com/example/designsystem/component/Dialog.kt
@@ -9,7 +9,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,6 +29,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.Dialog
 import com.example.core.designsystem.R
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
 
 /// Toast, Dialog
 @Composable
@@ -82,6 +92,51 @@ fun NonggleDialog(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModal(
+    modifier: Modifier = Modifier,
+    onDateSelected: (LocalDate) -> Unit,
+    onDismiss: () -> Unit,
+    datePickerState: DatePickerState
+) {
+    DatePickerDialog(
+        modifier = modifier.background(color = NonggleTheme.colorScheme.background),
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    val selectedDateMillis = datePickerState.selectedDateMillis
+                    if(selectedDateMillis != null) {
+                        val localDate = Instant.ofEpochMilli(selectedDateMillis)
+                            .atZone(ZoneId.of("Asia/Seoul"))
+                            .toLocalDate()
+                        onDateSelected(localDate)
+                    }
+                onDismiss()
+            }) {
+                Text(
+                    text = stringResource(R.string.dialog_Confirm_ButtonTitle),
+                    style = NonggleTheme.typography.b4_btn.copy(color = NonggleTheme.colorScheme.m1)
+                )
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text(
+                    text = stringResource(R.string.dialog_Cancel_ButtonTitle),
+                    style = NonggleTheme.typography.b4_btn.copy(color = NonggleTheme.colorScheme.g1)
+                )
+            }
+        }
+    ) {
+        DatePicker(
+            state = datePickerState,
+        )
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun DialogPreview() {
@@ -92,5 +147,17 @@ fun DialogPreview() {
         dialogContent = {
             Text(text = "다이얼로그 내용")
         }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun DatepickerDialog() {
+    val state = rememberDatePickerState()
+    DatePickerModal(
+        onDateSelected = {},
+        onDismiss = {},
+        datePickerState = state
     )
 }
