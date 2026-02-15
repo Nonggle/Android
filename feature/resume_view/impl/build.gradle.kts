415067d2a6f382258dab4b08899155e06ac5d10b
seunghee <victoria9981@naver.com>
2026-02-15 10:36:08 +0900

    feat: 이력서 열람 기능 제공 모듈 생성

---
+++ feature/resume_view/impl/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    alias(libs.plugins.example.nonggle.android.feature.ui)
+    alias(libs.plugins.example.nonggle.android.serialization)
+    alias(libs.plugins.example.nonggle.android.detekt)
+}
+
+android {
+    namespace = "com.nonggle.feature.resume_view.impl"
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(project(":core:ui"))
+}
