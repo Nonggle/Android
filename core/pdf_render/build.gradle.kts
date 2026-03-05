4cbf61aa520729a12faf2e43f02f21830ad314d3
seunghee <victoria9981@naver.com>
2026-03-05 22:09:53 +0900

    fix: 불필요 의존성 제거

--- core/pdf_render/build.gradle.kts
+++ core/pdf_render/build.gradle.kts
@@ -9,14 +9,13 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.coil.compose)
-    implementation(libs.coil.network.okhttp)
 }
